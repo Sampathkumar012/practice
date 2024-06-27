e419f298f33d4f0153e94679d37d0f2d4ebcbd51
neoteric <asampathkumar545@gmail.com>
2024-06-27 11:33:58 +0530

    first commit

---
+++ src/kkkk/Swiggy.java
@@ -0,0 +1,10 @@
+package kkkk;
+
+public class Swiggy {
+
+        public String name;
+        public String fatherName;
+
+    }
+
+
---
+++ src/kkkk/Test.java
@@ -0,0 +1,11 @@
+package kkkk;
+
+public class Test {
+    public static void main(String[] args) {
+        Swiggy swiggy=new Swiggy();
+        swiggy.name="Sampath";
+        swiggy.fatherName="Kodanda Ramulu";
+        System.out.println("name ="+swiggy.name);
+        System.out.println("fathername ="+swiggy.fatherName);
+    }
+}
