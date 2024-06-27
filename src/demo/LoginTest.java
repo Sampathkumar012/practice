f7362deab30fa8316fe3e0636002c0f53426db4b
neoteric <asampathkumar545@gmail.com>
2024-06-27 12:45:17 +0530

    commit login

---
+++ src/demo/LoginTest.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class LoginTest {
+    public static void main(String[] args){
+        Login login=new Login();
+        login.userName="samapth";
+        login.password="1234";
+    }
+}
